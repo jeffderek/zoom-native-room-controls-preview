668be4601cbe972e535aa93576444f3b95013e48
Jeff McAleer <JM13838@avispl.com>
2022-10-07 23:35:56 -0400

    feat: Updated the about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,19 +1,21 @@
 <template>
     <div class="about">
-        <h1>About</h1>
         <p>
-            This is an Alpha release of a image preview generator for Zoom
-            Native Room Controls. Contribute to the project
-            <a
-                href="https://github.com/jeffderek/zoom-native-room-controls-preview"
-                >on GitHub</a
-            >.
+            This is an Alpha release of a image preview generator for Zoom Native Room Controls. Contribute to the
+            project
+            <a href="https://github.com/jeffderek/zoom-native-room-controls-preview">on GitHub</a>.
         </p>
+        <p>This project is developed and maintained by <a href="https://www.jeffmcaleer.com">Jeff McAleer</a></p>
     </div>
 </template>
 
 <style lang="scss">
 .about {
     padding: 1rem;
+
+    p {
+        padding: 6px;
+        font-size: 1.2rem;
+    }
 }
 </style>
